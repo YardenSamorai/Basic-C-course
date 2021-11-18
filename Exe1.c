293bf85688d866e0ea7553c7ffacd6ac4c032d3b
Yarden Samorai <90088412+YardenSamorai@users.noreply.github.com>
2021-11-18 21:25:43 +0200

    Update Exe1.c

--- Exe1.c
+++ Exe1.c
@@ -3,59 +3,59 @@
 void main()
 {
 	//Q1
-	//printf("Hello Adi");
+	printf("Hello Adi");
 
 	//Q2
-	//double PI = 3.141592;
-	//printf("%.2f",PI);
+	double PI = 3.141592;
+	printf("%.2f",PI);
 
-	//Q3
-	//printf("   *\n  ***\n *****\n");
+	
+	printf("   *\n  ***\n *****\n");
 
 	//Q4
-	//int a, b;
-	//printf("please enter num1:\n");
-	//scanf("%d", &a);
-	//printf("please enter num2:\n");
-	//scanf("%d", &b);
-	//printf("%d * %d = %d\n", a, b, a * b);
-	//printf("%d / %d = %d\n", a, b, a / b);
-	//printf("%d % %d = %d", a, b, a % b);
+	int a, b;
+	printf("please enter num1:\n");
+	scanf("%d", &a);
+	printf("please enter num2:\n");
+	scanf("%d", &b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d / %d = %d\n", a, b, a / b);
+	printf("%d % %d = %d", a, b, a % b);
 
 	//Q5
-	//int a, b, c;
-	//printf("please enter num1:\n");
-	//scanf("%d", &a);
-	//printf("Please enter num2: \n");
-	//scanf("%d", &b);
-	//printf("a=%d, b=%d\n", a, b);
-	//c = a;
-	//a = b;
-	//b = c;
-	//printf("after swapping values : a=%d, b=%d", a,b);
+	int a, b, c;
+	printf("please enter num1:\n");
+	scanf("%d", &a);
+	printf("Please enter num2: \n");
+	scanf("%d", &b);
+	printf("a=%d, b=%d\n", a, b);
+	c = a;
+	a = b;
+	b = c;
+	printf("after swapping values : a=%d, b=%d", a,b);
 
 	//Q1 homelab
 
-	//int currentyear = 2021, sum;
-	//int age = 22;
-	//sum = currentyear - age;
-	//printf("The year of birth:%d\n", sum);
+	int currentyear = 2021, sum;
+	int age = 22;
+	sum = currentyear - age;
+	printf("The year of birth:%d\n", sum);
 
 	//Q2
-	//printf("*               *\n");
-	//printf(" *             *\n");
-	//printf("  *           *\n");
-	//printf("   *         *\n");
-	//printf("    *       *\n");
-	//printf("     *     *\n");
-	//printf("      *   *\n");
-	//printf("       * *\n");
-	//printf("      *   *\n");
-	//printf("     *     *\n");
-	//printf("    *       *\n");
-	//printf("   *         *\n");
+	printf("*               *\n");
+	printf(" *             *\n");
+	printf("  *           *\n");
+	printf("   *         *\n");
+	printf("    *       *\n");
+	printf("     *     *\n");
+	printf("      *   *\n");
+	printf("       * *\n");
+	printf("      *   *\n");
+	printf("     *     *\n");
+	printf("    *       *\n");
+	printf("   *         *\n");
 
-	//Q3
+	
 
 	long ID;
 	double hoursalary;
